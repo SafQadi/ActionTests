a0af24e637954ef5e701269bf71c102c4dc3877f
SafQadi <saf.qadi@gmail.com>
2023-11-05 01:44:29 +0100

    add matho cmake

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
-#include "main.hpp"
-
-int sumo(int a, int b)
-{
-  return a + b;
-}
+// this file shall be updated
+#include "matho.hpp"
 
 int main()
 {
+  auto ref = sumo(5, 3);
   std::cout << "Hello world\n";
+  std::cout << "ref: " << ref << std::endl;
 }

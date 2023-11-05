a9bd229b986395bc9098f75bd94e2b7e8b940508
SafQadi <saf.qadi@gmail.com>
2023-11-05 01:11:33 +0100

    added gtest

--- tests.cpp
+++ tests.cpp
@@ -1,12 +1,27 @@
 #include "gtest/gtest.h"
 
-TEST(MathTest, Plus)
+// TEST(MathTest, Plus)
+// {
+//     EXPECT_EQ(2 + 2, 4);
+// }
+
+// int main(int argc, char **argv)
+// {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
+
+int sumo(int a, int b)
 {
-    EXPECT_EQ(2 + 2, 4);
+  return a + b;
 }
 
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+class Testme : public testing::Test {
+ protected:
+  void SetUp() override {
+  }
+};
+
+TEST_F(Testme, sumoTest) {
+  EXPECT_EQ(sumo(2U, 5U), 7);
 }
